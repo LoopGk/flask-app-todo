8cc61062567f6a70a4040bf1a03262313563ee09
h-rub <rubio@loopgk.com>
2022-04-02 13:59:22 -0600

    Se añadieron nuevas rutas y modelos

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,22 @@
+from datetime import datetime
 from todo import db
 
-class Cliente(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nombre = db.Column(db.String(80), nullable = False)
-    apellido = db.Column(db.String(80), nullable = False)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(80), nullable = False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.id}')"
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    isDone = db.Column(db.Boolean)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
     def __repr__(self):
-        return f"<User {self.nombre}>"
+        return f"Task('{self.title}', '{self.content}')"
--- todo/routes.py
+++ todo/routes.py
@@ -1,16 +1,72 @@
 from crypt import methods
-from flask import render_template
+import re
+from flask import render_template, request, jsonify
 from todo import app, db
-from todo.models import Cliente
+from todo.models import User, Task
+
+PATH_BASE_API = "/api/v1"
 
 # Rutas
 @app.route("/")
 def index():
     return render_template("hola.html", title = "Home")
 
-@app.route("/delete/", methods = ["POST"])
-def delete():
-    _id = 2
-    Cliente.query.filter_by(id = _id).delete()
-    db.session.commit()
-    return {"msg": "Cliente eliminado"}
+# API routes
+
+@app.route(f'{PATH_BASE_API}/user/create', methods=['GET', 'POST'])
+def create_user():
+    if request.method == "POST":
+        data = request.json
+        username = data["username"]
+        email = data["email"]
+        password = data["password"]
+
+        user = User(username = username, email = email, password = password)
+        db.session.add(user)
+        db.session.commit()
+        msg = "Usuario creado"
+    elif request.method == 'GET':
+        msg = 'Este endpoint es para el registro de un nuevo usuario. Utilice el método POST'
+    return {
+        "code": 200,
+        "msg": msg
+    }
+
+@app.route(f'{PATH_BASE_API}/users/all', methods=['GET'])
+def get_users():
+    response = []
+    list_users = User.query.all()
+    # "Serializar"
+    for user in list_users:
+        data = {"username":user.username, "email": user.email}
+        response.append(data)
+    return jsonify(response)
+
+@app.route(f'{PATH_BASE_API}/task/create/<int:user_id>', methods=['GET', 'POST'])
+def create_task(user_id):
+    if request.method == 'POST':
+        data = request.json
+        title = data["title"]
+        content = data["content"]
+        is_done = data["isDone"]
+        user_id = user_id
+
+        task = Task(title = title, content = content, isDone = is_done, user_id = user_id)
+        db.session.add(task)
+        db.session.commit()
+        msg = "Tarea creada exitosamente"
+    elif request.method == 'GET':
+        msg = 'Este endpoint es para la cración de una nueva tarea. Utilice el método POST'
+    return {
+        "code": 200,
+        "msg": msg
+    }
+
+@app.route(f'{PATH_BASE_API}/task/getmytasks/<int:user_id>', methods=['GET'])
+def get_my_tasks(user_id):
+    response = []
+    tasks = Task.query.filter_by(user_id = user_id)
+    for task in tasks:
+        data = {"id": task.id, "title": task.title, "content": task.content, "isDone": task.isDone, "date_posted": task.date_posted}
+        response.append(data)
+    return jsonify(response)
